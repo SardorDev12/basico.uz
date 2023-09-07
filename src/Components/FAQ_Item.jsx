3ca8a7036d2c64cbc6f1de21f992e8d66eb4e0e3
Sardor <sardoribnfarhod@gmail.com>
2023-09-07 17:46:49 +0500

    FAQ SECTION

--- src/Components/FAQ_Item.jsx
+++ src/Components/FAQ_Item.jsx
@@ -1,18 +1,28 @@
+import React, { useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
 
 function FAQ_Item({ question, answer }) {
+  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
+
+  const toggleAnswer = () => {
+    setIsAnswerVisible(!isAnswerVisible);
+  };
+
   return (
     <>
       <tr>
-        <th className="question">
-          <FaChevronDown className="down-arrow" />
+        <FaChevronDown />
+        <th onClick={toggleAnswer} className="faq-question">
           {question}
         </th>
       </tr>
       <tr>
-        <td className="answer">{answer}</td>
+        <td className={`answer ${isAnswerVisible ? "visible" : "hide"}`}>
+          {isAnswerVisible && answer}
+        </td>
       </tr>
     </>
   );
 }
+
 export default FAQ_Item;
