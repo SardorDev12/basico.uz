0d356442ec855c161718460ff3ad6f00d2571fc9
Sardor <sardoribnfarhod@gmail.com>
2023-09-07 18:33:27 +0500

    FAQ SECTION fix 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Faq from "./Components/Faq";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Qualities from "./Components/Qualities";
 import Services from "./Components/Services";
+import Faq from "./Components/Faq";
 
 function App() {
   return (
