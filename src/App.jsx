ad28e1e1ed02460b5fbccedeab4ddb91b5cb6945
Sardor <sardoribnfarhod@gmail.com>
2023-09-08 12:30:51 +0500

    fix faq section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Qualities from "./Components/Qualities";
 import Services from "./Components/Services";
-import Faq from "./Components/Faq";
+import Faq from "./Components/FaqSection";
 import Statistics from "./Components/Statistics";
 import Portfolio from "./Components/Portfolio";
 import Partners from "./Components/Partners";
