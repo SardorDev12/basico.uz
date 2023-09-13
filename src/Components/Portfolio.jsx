4743535b7c3eb875443e854479ced8cd03a1fbcc
Sardor <sardoribnfarhod@gmail.com>
2023-09-13 16:18:09 +0500

    fetch setup

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,35 +1,41 @@
 import "../Sass/portfolio.scss";
 import image1 from "../assets/Images/Portfolio/portfoilo (1).jpg";
 import image2 from "../assets/Images/Portfolio/portfoilo (2).jpg";
-import image3 from "../assets/Images/Portfolio/portfoilo (3).jpg";
 import image4 from "../assets/Images/Portfolio/portfoilo (4).jpg";
 import image5 from "../assets/Images/Portfolio/portfoilo (5).jpg";
 import image6 from "../assets/Images/Portfolio/portfoilo (6).jpg";
 import image7 from "../assets/Images/Portfolio/portfoilo (7).jpg";
-import image8 from "../assets/Images/Portfolio/portfoilo (8).jpg";
 import image9 from "../assets/Images/Portfolio/portfoilo (9).jpg";
-import image10 from "../assets/Images/Portfolio/portfoilo (10).jpg";
-import image11 from "../assets/Images/Portfolio/portfoilo (11).jpg";
-import image12 from "../assets/Images/Portfolio/portfoilo (12).jpg";
 import image13 from "../assets/Images/Portfolio/portfoilo (13).jpg";
 import image14 from "../assets/Images/Portfolio/portfoilo (14).jpg";
 import image15 from "../assets/Images/Portfolio/portfoilo (15).jpg";
 import image16 from "../assets/Images/Portfolio/portfoilo (16).jpg";
 import image18 from "../assets/Images/Portfolio/portfoilo (18).jpg";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Portfolio() {
+  // const [portfoilo, setPortfolio] = useState([]);
+  // useEffect(async () => {
+  //   const res = await fetch(url);
+  //   const data = res.json();
+  //   setPortfolio(data);
+  // });
   return (
     <section className="portfolio-section" id="portfolio">
       <h2 className="section-title">Наши Кейсы</h2>
       <div className="section-container">
+        {/* Dynamic setup */}
+        {/* {portfoilo.map((img) => {
+          return (
+            <div className="portfolio-item">
+              <img src={img} alt="portfolio-image" />
+            </div>
+          );
+        })} */}
         <div className="portfolio-item">
           <img src={image1} alt="portfolio-image" />
         </div>
-
-        {/* <div className="portfolio-item">
-          <img src={image3} alt="portfolio-image" />
-        </div> */}
-
         <div className="portfolio-item">
           <img src={image5} alt="portfolio-image" />
         </div>
@@ -39,24 +45,12 @@ function Portfolio() {
         <div className="portfolio-item">
           <img src={image7} alt="portfolio-image" />
         </div>
-        {/* <div className="portfolio-item">
-          <img src={image8} alt="portfolio-image" />
-        </div> */}
         <div className="portfolio-item">
           <img src={image2} alt="portfolio-image" />
         </div>
         <div className="portfolio-item">
           <img src={image9} alt="portfolio-image" />
         </div>
-        {/* <div className="portfolio-item">
-          <img src={image10} alt="portfolio-image" />
-        </div> */}
-        {/* <div className="portfolio-item">
-          <img src={image11} alt="portfolio-image" />
-        </div> */}
-        {/* <div className="portfolio-item">
-          <img src={image12} alt="portfolio-image" />
-        </div> */}
         <div className="portfolio-item">
           <img src={image13} alt="portfolio-image" />
         </div>
@@ -75,9 +69,6 @@ function Portfolio() {
         <div className="portfolio-item">
           <img src={image18} alt="portfolio-image" />
         </div>
-        {/* <div className="portfolio-item">
-          <img src={image17} alt="portfolio-image" />
-        </div> */}
       </div>
     </section>
   );
